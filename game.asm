3d4df4d51cc5493208f9f896f7c86de548e56159
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2023-11-13 14:47:35 +0100

    updates

--- game.asm
+++ game.asm
@@ -2,7 +2,7 @@
   .ineschr 1   ; 1x  8KB CHR data
   .inesmap 0   ; mapper 0 = NROM, no bank swapping
   .inesmir 1   ; background mirroring
-  
+
 
 ;;;;;;;;;;;;;;;
 ;; DECLARE SOME VARIABLES HERE
@@ -11,11 +11,9 @@
 player_x  .rs 1  ; .rs 1 means reserve one byte of space
 player_y  .rs 1  
 boost .rs 1
-fire .rs 1
-proj_x .rs 1
-proj_y .rs 1
-
-
+fired .rs 1
+;proj_x .rs 1
+;proj_y .rs 1
 
 ;;;;;;;;;;;;;;;
 
@@ -37,17 +35,20 @@ vblankwait1:       ; First wait for vblank to make sure PPU is ready
   BIT $2002        ; Check value of $2002 and set CPU flag for Zero, Negative, Overflow 
 				   ; accordingly without affecting Accumulator
   BPL vblankwait1  ; If Negative flag is clear, branch to vblankwait1
-
+  
+  LDX #$00
 clrmem:
   LDA #$00
   STA $0000, x
   STA $0100, x
+  STA $0200, x
+  STA $0300, x
   STA $0400, x
   STA $0500, x
   STA $0600, x
   STA $0700, x
-  LDA #$FE
-  STA $0300, x
+;  LDA #$FE
+;  STA $0300, x
   INX
   BNE clrmem
    
@@ -55,7 +56,8 @@ vblankwait2:      ; Second wait for vblank, PPU is ready after this
   BIT $2002       ; Check value of $2002 and set CPU flag for Zero, Negative, Overflow 
 				  ; accordingly without affecting Accumulator
   BPL vblankwait2 ; If Negative flag is clear, branch to vblankwait2
-  
+   
+   
 LoadPalette:
   LDA $2002             ; read PPU status to reset the high/low latch
   LDA #$3F
@@ -80,7 +82,7 @@ LoadSpritePaletteLoop:
   LDA sprite_palette, x     ;load palette byte
   STA $2007					;write to PPU
   INX                   	;set index to next byte
-  CPX #$14            
+  CPX #$10            
   BNE LoadSpritePaletteLoop  ;if x = $10, all done
 
   LDX #$00              ; start at 0
@@ -88,11 +90,9 @@ LoadSpritesLoop:
   LDA sprites, x        ; load data from address (sprites +  x)
   STA $0200, x          ; store into RAM address ($0200 + x)
   INX                   ; X = X + 1
-  CPX #$14              ; Compare X to hex $10, decimal 16. loads the first 16 bytes of sprites (4 sprites)
+  CPX #$14              ; Compare X to hex $14, decimal 20. loads the first 20 bytes of sprites (5 sprites)
   BNE LoadSpritesLoop   ; Branch to LoadSpritesLoop if compare was Not Equal to zero
                         ; if compare was equal to zero, keep going down 
-
-  
   
   LDA #%10000000   ; enable NMI, sprites from Pattern Table 0
   STA $2000
@@ -102,20 +102,62 @@ LoadSpritesLoop:
   
   LDA $0203
   STA player_x
-  
   LDA $0200
   STA player_y
-  
-
-
+ 
 Foreverloop:
   JMP Foreverloop     ;jump back to Forever, infinite loop
+
+init_apu:
+  ldx #$00
+ ; initialize sound registers 
+initloop:
+  lda #$00
+  sta $4000, x
+  inx
+  cpx #$14
+  bne initloop
+
+  lda #$00
+  sta $4015
+  lda #$0F
+  sta $4015
+; setup apu frame counter
+  lda #$40
+  sta $4017  
+  rts
 
 NMI: 
   LDA #$00
   STA $2003       ; set the low byte (00) of the RAM address
   LDA #$02
   STA $4014       ; set the high byte (02) of the RAM address, start the transfer
+ 
+; if it has exceeded x position of xxx, reset the missile
+  LDA $0213 ; x coordinates
+  CMP #$F0 ; 
+  BEQ reset
+ 
+;if it has been fired, add 1 to x position of projectile $0213
+  LDA fired
+  BEQ nmi_end ; branch if equal to zero, branch to nmi_end if it hasnt been fired
+  LDA $0213
+  CLC
+  ADC #$1 ; if it has been fired, move the missile to the right
+  STA $0213
+  JMP nmi_end
+
+;move the missile to the default position. reset the fired variable to 0 
+reset:
+  jsr init_apu
+  LDA #$CD ;CD = 205
+  STA $0213 ; x coordinates
+  LDA #$DC ; DC = 220
+  STA $0210 ; y coordinates
+  LDA #$0 ; 0
+  STA fired ; stored in fired variable
+
+nmi_end:
 
 LatchController:
   LDA #$01        ; store 1 in acumulator
@@ -131,7 +173,7 @@ ReadA:
   BEQ ReadADone   ; branch to ReadADone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
 ;boost  
-  LDA #$1
+  LDA #$1 ; set boost to 1
   STA boost
 
 ReadADone:        ; handling this button is done
@@ -141,25 +183,26 @@ ReadB:
   AND #%00000001  ; only look at bit 0
   BEQ ReadBDone   ; branch to ReadBDone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
-
-;fire
+;fire logic
+  LDA fired ; load value if it has been fired
+  BNE ReadBDone ; branch if not equal to zero (if it has been fired, go to readbdone), otherwise, continue
   LDA #$1	; load #$1 into accumulator
-  STA fire	; store 1 in variable "fire"
+  STA fired	; store 1 in variable "fired"
+ 
+  lda #138
+  sta $400A
+
+  lda #139
+  sta $400B
+
+  lda #%11000000
+  sta $4008
+  sta $4017
   
-  LDA proj_x ; load current position (should be off screen initially)
   LDA player_x ; load x coordinates of player
   STA $0213 ; store in x coordinates of sprite 5
-  STA proj_x ; store x coordinates of projectile
-  TAX ;transfer a to x
-  INX ; x +1
-  INX ; x +1
-  STX proj_x ; store new x in proj_x
-  
-  LDA proj_y ; load y position (should be off screen initially)
   LDA player_y ; load y coordinates of player
   STA $210 ; store y position of projectile
-  STA proj_y ; store y coordinates of projectile
-  
 
 ReadBDone:        ; handling this button is done
 
@@ -182,17 +225,22 @@ ReadUp:
   AND #%00000001  ; only look at bit 0
   BEQ ReadUpDone   ; branch to ReadUpDone if button is NOT pressed (0)
 
-; move guy up  
+; move guy up 
   LDA player_y
-  STA $0200
-  STA $0204
-  TAX
-  CLC
-  ADC #$08
-  STA $0208
-  STA $020C
-  DEX
-  STX player_y 
+  ;CMP #$0F ; ; if it has reached position x of xxx, ignore further movement in y axis upwards
+  SBC #$5
+  BEQ ReadUpDone ; if equal to zero, go to readupdone
+  
+  LDA player_y
+  STA $0200 ; y of sprite 1
+  STA $0204 ; y of sprite 2
+  TAX ; transfer a to x
+  CLC ; clear carry
+  ADC #$08 ; account for shift of tile location in *.chr . add #$8 to x
+  STA $0208 ; y coordinates of sprite 3
+  STA $020C ; y coordinates if sprite 4
+  DEX ; decrease x by 1
+  STX player_y  ; store new x as new y coordinates for player sprite
 
 ReadUpDone:
 
@@ -203,6 +251,10 @@ ReadDown:
 
 ; move guy down  
   LDA player_y
+  CMP #$DB ; ; if it has exceeded y position of xxx, ignore further movement in y axis downwards
+  BEQ ReadDownDone ; if equal to zero, go to readdowndone
+
+;  LDA player_y
   STA $0200
   STA $0204
   TAX
@@ -220,6 +272,11 @@ ReadLeft:
   AND #%00000001  ; only look at bit 0
   BEQ ReadLeftDone   ; branch to ReadLeftDone if button is NOT pressed (0)
 ;Move guy to the left 
+  LDA player_x
+  ;CMP #$0F ; ; if it has reached position x of xxx, ignore further movement in x axis to the left
+  SBC #$5
+  BEQ ReadLeftDone ; if equal to zero, go to readleftdone
+  
   LDA player_x
   STA $0203
   STA $020B
@@ -239,6 +296,9 @@ ReadRight:
   BEQ ReadRightDone ; branch to ReadRightDone if button is NOT pressed (0)
 ;Move guy to the right  
   LDA player_x
+  CMP #$F0 ; ; if it has exceeded x position of xxx, ignore further movement in x axis to the right
+  BEQ ReadRightDone ; if equal to zero, go to readrightdone
+    
   STA $0203
   STA $020B
   TAX
@@ -254,7 +314,7 @@ ReadRight:
   BNE ReadRightDone
   INX
   STX player_x
-  LDA #$0
+  LDA #$0 ; reset boost to zero
   STA boost  
   
 ReadRightDone:
@@ -273,7 +333,7 @@ background_palette:
   
 sprite_palette:
   .db $22,$0F,$00,$15	;sprite palette 1
-  .db $22,$16,$27,$28	;sprite palette 5
+  .db $22,$16,$27,$28	;sprite palette 2 - fire
   
   .db $22,$1A,$30,$27	;sprite palette 2
   .db $22,$16,$30,$27	;sprite palette 3
@@ -282,12 +342,12 @@ sprite_palette:
 	
 sprites:
 
-     ;vert tile attr horiz
-  .db $18, $0A, %00000000, $08   ;sprite 1
-  .db $18, $0B, %00000000, $10   ;sprite 2
-  .db $20, $1A, %00000000, $08   ;sprite 3
-  .db $20, $1B, %00000000, $10   ;sprite 4
-  .db $50, $0E, %00000001, $50 ; sprite 5
+       ;y,tile, attribute,   x
+  .db $18, $0A, %00000000, $08   ;sprite 1/4: player
+  .db $18, $0B, %00000000, $10   ;sprite 2/4: player
+  .db $20, $1A, %00000000, $08   ;sprite 3/4: player
+  .db $20, $1B, %00000000, $10   ;sprite 4/4: player
+  .db $DC, $0E, %00000001, $CD ; sprite 5: projectile (y 220, x 205)
 
 ;  76543210
 ;  ||||||||
@@ -310,4 +370,21 @@ sprites:
 
   .bank 2
   .org $0000
-  .incbin "game.chr"   ;includes 8KB graphics file from SMB1
+  .incbin "game.chr"   ;includes 8KB graphics file from SMB1 (tm)
+  .incbin "game.nsf"   ; includes nsf audio file from Super C (tm)
+  
+  ;  * No expansion chip
+;Building music data...
+; * Sequences used: 0 (0 bytes)
+; * Instruments used: 1 (1 bytes)
+; * Song 1, 145 patterns (582 bytes), 29 frames (348 bytes)
+;
+; * Samples located at: $C000
+; * DPCM samples used: 0 (0 bytes)
+; * NSF load address: $A745
+;Writing output file...
+; * Driver size: 5128 bytes
+; * Song data size: 955 bytes (3%)
+; * NSF type: Linear (driver @ $AB00)
+;Done, total file size: 6211 bytes
+
---
+++ sound/music.asm
@@ -0,0 +1,144 @@
+; Dn-FamiTracker exported music data: melody5.dnm
+;
+
+; Module header
+	.word ft_song_list
+	.word ft_instrument_list
+	.word ft_sample_list
+	.word ft_samples
+	.word ft_groove_list
+	.byte 0 ; flags
+	.word 3600 ; NTSC speed
+	.word 3000 ; PAL speed
+
+; Instrument pointer list
+ft_instrument_list:
+	.word ft_inst_0
+	.word ft_inst_1
+	.word ft_inst_2
+
+; Instruments
+ft_inst_0:
+	.byte 0
+	.byte $11
+	.word ft_seq_2a03_0
+	.word ft_seq_2a03_4
+
+ft_inst_1:
+	.byte 0
+	.byte $01
+	.word ft_seq_2a03_5
+
+ft_inst_2:
+	.byte 0
+	.byte $05
+	.word ft_seq_2a03_15
+	.word ft_seq_2a03_2
+
+; Sequences
+ft_seq_2a03_0:
+	.byte $40, $FF, $00, $00, $0D, $0C, $0C, $0C, $0C, $0C, $0C, $0C, $0C, $0B, $0B, $0B, $0B, $0B, $0B, $0B
+	.byte $0B, $0B, $0B, $0B, $0B, $0B, $0A, $0A, $0A, $0A, $0A, $0A, $09, $09, $09, $09, $09, $08, $08, $08
+	.byte $08, $08, $08, $07, $07, $07, $07, $06, $06, $06, $05, $05, $05, $04, $04, $03, $03, $02, $01, $01
+	.byte $01, $00, $00, $00, $00, $00, $00, $00
+ft_seq_2a03_2:
+	.byte $08, $FF, $00, $00, $03, $01, $0E, $24, $2E, $FF, $F3, $DC
+ft_seq_2a03_4:
+	.byte $02, $FF, $00, $00, $02, $02
+ft_seq_2a03_5:
+	.byte $06, $FF, $00, $00, $0C, $0B, $09, $04, $00, $00
+ft_seq_2a03_15:
+	.byte $08, $FF, $00, $00, $0E, $0E, $0D, $0C, $0A, $07, $01, $00
+
+; DPCM instrument list (pitch, sample index)
+ft_sample_list:
+
+; DPCM samples list (location, size, bank)
+ft_samples:
+
+; Groove list
+ft_groove_list:
+	.byte $00
+; Grooves (size, terms)
+
+; Song pointer list
+ft_song_list:
+	.word ft_song_0
+	.word ft_song_1
+	.word ft_song_2
+
+; Song info
+ft_song_0:
+	.word ft_s0_frames
+	.byte 1	; frame count
+	.byte 64	; pattern length
+	.byte 6	; speed
+	.byte 150	; tempo
+	.byte 0	; groove position
+	.byte 0	; initial bank
+
+ft_song_1:
+	.word ft_s1_frames
+	.byte 1	; frame count
+	.byte 64	; pattern length
+	.byte 6	; speed
+	.byte 150	; tempo
+	.byte 0	; groove position
+	.byte 0	; initial bank
+
+ft_song_2:
+	.word ft_s2_frames
+	.byte 1	; frame count
+	.byte 64	; pattern length
+	.byte 6	; speed
+	.byte 150	; tempo
+	.byte 0	; groove position
+	.byte 0	; initial bank
+
+
+;
+; Pattern and frame data for all songs below
+;
+
+; Bank 0
+ft_s0_frames:
+	.word ft_s0f0
+ft_s0f0:
+	.word ft_s0p0c0, ft_s0p0c1, ft_s0p0c1, ft_s0p0c3, ft_s0p0c1
+; Bank 0
+ft_s0p0c0:
+	.byte $82, $01, $E0, $25, $29, $25, $2A, $25, $2C, $25, $2A, $25, $2C, $25, $30, $25, $33, $25, $2C, $25
+	.byte $2E, $25, $29, $25, $2A, $25, $2C, $29, $2E, $29, $2C, $25, $2A, $25, $83, $2E, $01
+
+; Bank 0
+ft_s0p0c1:
+	.byte $00, $3F
+
+; Bank 0
+ft_s0p0c3:
+	.byte $E1, $18, $03, $18, $03, $18, $03, $82, $01, $18, $1C, $1A, $19, $17, $15, $1C, $18, $18, $18, $18
+	.byte $18, $18, $18, $82, $00, $18, $18, $18, $18, $83, $18, $01, $18, $00, $18, $00, $18, $00, $18, $01
+	.byte $18, $00, $18, $00, $18, $01, $18, $00, $18, $00, $18, $01, $18, $00, $18, $00, $18, $02
+
+; Bank 0
+ft_s1_frames:
+	.word ft_s1f0
+ft_s1f0:
+	.word ft_s0p0c1, ft_s1p0c1, ft_s0p0c1, ft_s0p0c1, ft_s0p0c1
+; Bank 0
+ft_s1p0c1:
+	.byte $82, $00, $E2, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.byte $83, $3F, $08, $82, $00, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F, $3F
+	.byte $3F, $3F, $83, $3F, $14
+
+; Bank 0
+ft_s2_frames:
+	.word ft_s2f0
+ft_s2f0:
+	.word ft_s0p0c1, ft_s0p0c1, ft_s0p0c1, ft_s2p0c3, ft_s0p0c1
+; Bank 0
+ft_s2p0c3:
+	.byte $E0, $15, $00, $18, $00, $18, $3D
+
+
+; DPCM samples (located at DPCM segment)
