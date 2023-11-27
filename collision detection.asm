316aa0feba716973897be08a6a745e393c85e27c
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2023-11-27 13:31:03 +0100

    Update collision detection.asm

--- collision detection.asm
+++ collision detection.asm
@@ -4,6 +4,7 @@ define y2 $10
 define x2 $11
 define ycollision $50
 define xcollision $51
+sec
 
 store:
 lda #$4
@@ -16,21 +17,18 @@ lda #$1
 sta x2 ; (0-255)
 
 calc_y:
-clc
 lda y1
 sbc y2
 sta $20
 cmp #$2
 bcc yless2
 
-clc
 lda y2
 sbc y1
 sta $30
 cmp #$2
 bcc yless2
 
-clc
 lda y1
 cmp y2
 beq yless2 
@@ -42,21 +40,18 @@ lda #$1
 sta ycollision
 
 calc_x:
-clc
 lda x1
 sbc x2
 sta $21
 cmp #$2
 bcc xless2
 
-clc
 lda x2
 sbc x1
 sta $31
 cmp #$2
 bcc xless2
 
-clc
 lda x1
 cmp x2
 beq xless2
