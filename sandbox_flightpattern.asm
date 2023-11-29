b6df12f93343e81dd15892cca80b7fb450bf03d8
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2023-11-29 11:06:27 +0100

    Update sandbox_flightpattern.asm

--- sandbox_flightpattern.asm
+++ sandbox_flightpattern.asm
@@ -1,25 +1,33 @@
-enemy_y = 0
-enemy_x = ff
-define pattern1 = #$0
+enemy1_y = 0
+enemy1_x = fa
+enemy2_y = 5
+enemy2_x = ff
+enemy1pattern = #$3c
+enemy2pattern = #$64
 
+define enemy1_homing $30
+define enemy2_homing $31
+define pattern1count $40 ; depth of swoop
+define pattern2count $41 ; depth of swoop
+
+lda #$0
+sta pattern1count  ; index for pattern loop
+sta pattern2count  ; index for pattern loop
 
 start:
-lda enemy_homing ; is enemy homing?
-bne skippattern ; yes, skip pattern
-ldy pattern1
-cmp #$3c   ; is A less than 60?
-bcs skippattern: no
-inc enemy_y
-inc pattern1
+lda enemy1_homing  ; is enemy homing?
+bne skippattern    ; yes, skip pattern
+ldy pattern1count
+cmp enemy1pattern  ; is A less than 60?
+bcs skippattern:   ; no, branch to skippattern
+inc enemy1_y       ; follow pattern
+inc pattern1count  ; increase pattern count
 
 
-
-
-
-ontarget_reset:
+ontarget_reset:   ; reset after target has passed
 lda #$0
 sta enemy_homing  ; reset enemy homing
-sta pattern1      ; reset pattern1 count
+sta pattern1count ; reset pattern1 count
 
 ontarget:
 
