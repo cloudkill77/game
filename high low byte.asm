f3c59ae0709d904666aea01189f5b59e79d84b7c
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2023-12-03 12:34:27 +0100

    Update high low byte.asm

--- high low byte.asm
+++ high low byte.asm
@@ -11,7 +11,16 @@ LoadSprites1Loop:
   STA [oamLo], y          ; store into RAM address 
   INY                   ;
   CPY #$10              ; 
-  BNE LoadSprites1Loop   ; Branch to LoadSpritesLoop if compare was Not Equal to zero
+  BNE LoadSprites1Loop   ; Branch to LoadSprites1Loop if compare was Not Equal to zero
                         ; if compare was equal to zero, keep going down
   lda #$10
   sta oamLo            ; advance address for next sprite
+
+  LDY #$00
+LoadSprites2Loop:
+  LDA sprites, y        ; load data from address
+  STA [oamLo], y          ; store into RAM address 
+  INY                   ;
+  CPY #$ef              ; 
+  BNE LoadSprites2Loop   ; Branch to LoadSprites2Loop if compare was Not Equal to zero
+                        ; if compare was equal to zero, keep going down
