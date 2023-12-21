ff249bac7b97e8bfe5589468c5e9a0f9487db5ba
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2023-12-21 10:56:11 +0100

    Update to 16 bit addition.asm

--- 16 bit addition.asm
+++ 16 bit addition.asm
@@ -1,19 +1,20 @@
+; add an 8 bit number to a 16 bit number which is stored as "high" and "low" bytes in $00 and $01 respectively. if an overflow occurs after addition to the low byte, a carry flag is set. this carry flag then gets added to the high byte by adding #$00 with carry (ADC #$00).
 
-define highbyte $0
-define lowbyte $1
+define highbyte $00
+define lowbyte $01
 
-lda #$1
-sta highbyte  ; 2*256 = 512
+lda #$01
+sta highbyte     ; eg 2*256 = 512
 lda #$40
 sta lowbyte
 
 start:
 LDA lowbyte      ; load low 8 bits of 16 bit value
 CLC              ; clear carry
-ADC #$60        ; add 1
+ADC #$60         ; add #$60 (for example)
 STA lowbyte      ; done with low bits, save back
 LDA highbyte     ; load upper 8 bits
 ADC #$00         ; add 0 and carry from previous add
 STA highbyte     ; save back
 
-jmp start
+jmp start        ; keep adding value infinitely
