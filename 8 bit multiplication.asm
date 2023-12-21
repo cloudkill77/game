1a424e541ea1f6608eec907b07fd17cae067c08d
cloudkill77 <150142302+cloudkill77@users.noreply.github.com>
2023-12-21 11:05:59 +0100

    Update 8 bit multiplication.asm

--- 8 bit multiplication.asm
+++ 8 bit multiplication.asm
@@ -1,19 +1,23 @@
+; multiplication of two 8 bit numbers ($00 and $01), storing result in a 16 bit number stored as high and low bytes ($10 and $11 respectively)
+; multiplication is performed by adding factor 1 to itself factor 2 number of times. ie 4x3 = 0+4+4+4 = 12
+; if an overflow occurs on the low byte, a carry of value 1 is set, it then gets added to the high byte. if no overflow occured, no carry is added.
+
 lda #$ff
-sta $0     ; factor 1
+sta $00    ; factor 1
 lda #$3
-sta $1     ; factor 2
+sta $01    ; factor 2
 
 clc
-ldx #$0
+ldx #$00
 loop:
 lda $11    ; load low byte result
-adc $0     ; add factor 1
+adc $00    ; add factor 1
 sta $11    ; store as low byte result
 inx        ; increment x
-lda #$0    ; load 0
-adc $10    ; add carry to high byte
+lda #$00   ; load 0
+adc $10    ; add carry (if present) to high byte
 sta $10    ; store as high byte result
-cpx $1     ; compare x against factor 2
+cpx $01    ; compare x against factor 2
 bne loop   ; loop if x is less than factor 2
 
 brk
